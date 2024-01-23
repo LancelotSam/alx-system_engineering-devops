f98b4acdb53ffc5fe27d571da41fc3480055e776
Lancelot <skmburu110@gmail.com>
2024-01-22 19:02:28 -0800

    using puppet, puppet-lint, puppet-lint --fix and puppet apply

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env puppet
 #using puppet to install a package from pip
 
-package { 'flask':
+package { ['flask', 'Werkzeug']:
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug-2.1.1':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
