947e0fc3bf9ca2716a376070bce8d8d8522835d1
Lancelot <skmburu110@gmail.com>
2024-01-22 19:31:42 -0800

    changed the version of werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,12 @@
 #!/usr/bin/env puppet
 #using puppet to install a flask package from pip
 
-package { ['flask', 'Werkzeug']:
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
-package { 'Werkzeug-2.1.1':
+package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
 }
